8b81a2d314594f0702b5b2088aefcfe8236c618d
terlek <terlek@DESKTOP-RL37VNN>
2023-02-06 10:07:20 +0000

    Added failing tests

---
+++ test/CsvParserTests.cs
@@ -0,0 +1,51 @@
+using Csv_Parser.models;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Csv_Parser.tests;
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CsvDataModel_GivenCorrectParameters_ReturnsCorrectProperties()
+    {
+        var csvData = new FileData()
+        {
+            Name = "Test Data",
+            Description = "Test Description",
+            Quantity = "1"
+        };
+
+        Assert.IsInstanceOf<FileData>(csvData);
+        Assert.That(!string.IsNullOrEmpty(csvData.Name));
+        Assert.That(!string.IsNullOrEmpty(csvData.Description));
+        Assert.That(!string.IsNullOrEmpty(csvData.Quantity));
+        Assert.That(csvData.Name, Is.SameAs("Test Data"));
+        Assert.That(csvData.Description, Is.SameAs("Test Description"));
+        Assert.That(csvData.Quantity, Is.SameAs("1"));
+    }
+    [Test]
+    public void CsvParser_GivenCorrectPathParameter_ReturnsList()
+    {
+        var path = "CsvFile.csv";
+        var csvDataParser = new CsvParser(path);
+
+        var objectList = csvDataParser.ExtractDataFromFile();
+
+        Assert.IsInstanceOf<List<FileData>>(objectList);
+    }
+    [Test]
+    public void CsvParser_GivenInCorrectPathParameter_ReturnsException()
+    {
+        var path = "Incorrect path";
+        var csvDataParser = new CsvParser(path);
+
+        Assert.That(() => csvDataParser.ExtractDataFromFile(), Throws.TypeOf<InvalidFileTypeException>());
+    }
+
+}
